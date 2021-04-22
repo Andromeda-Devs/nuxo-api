c698d55d83ab9f56ff31022800f8d3405d471c3c
issaiassalas <issaiassalas@gmail.com>
2021-04-22 18:34:56 -0400

    create document

--- api/scrapper/services/scrapper.js
+++ api/scrapper/services/scrapper.js
@@ -2,13 +2,73 @@
 const puppeteer = require('puppeteer');
 
 const scraperObj = {
-  url: 'https://zeusr.sii.cl/AUT2000/InicioAutenticacion/IngresoRutClave.html?https://www1.sii.cl/cgi-bin/Portal001/mipeSelEmpresa.cgi?DESDE_DONDE_URL=OPCION%3D2%26TIPO%3D4',
-  uniqueRoute: 'https://www1.sii.cl/cgi-bin/Portal001/mipeAdminDocsEmi.cgi?RUT_RECP=&FOLIO=&RZN_SOC=&FEC_DESDE=&FEC_HASTA=&TPO_DOC=&ESTADO=&ORDEN=&NUM_PAG=1',
-  multiRoute: 'https://www1.sii.cl/cgi-bin/Portal001/mipeSelEmpresa.cgi?DESDE_DONDE_URL=OPCION%3D2%26TIPO%3D4',
+  multiRoute: '',
   next: 'a[title="Pagina siguiente"]',
-  async setUrl(url) {
-    this.url = url;
-    return this.url;
+  tags: {
+    sender: {
+      businessName: 'input[name="EFXP_RZN_SOC"]',
+      address: 'select[name="EFXP_DIR_ORIGEN"]',
+      hideEmail: 'input[name="FONO_SI_NO"]',
+      commune: 'input[name="EFXP_CMNA_ORIGEN"]',
+      city: 'input[name="EFXP_CIUDAD_ORIGEN"]',
+      saleType: 'select[name="EFXP_TIPOVENTA_SELECT"]',
+      email: 'input[name="EFXP_EMAIL_EMISOR"]',
+      phone: 'input[name="EFXP_FONO_EMISOR"]',
+      concept: 'input[name="EFXP_GIRO_EMIS"]',
+      economicActivity: 'select[name="EFXP_ACTECO_SELECT"]'
+    },
+    receiver: {
+      rut: {
+        rut: 'input[name="EFXP_RUT_RECEP"]',
+        dv: 'input[name="EFXP_DV_RECEP"]'
+      },
+      businessName: 'input[name="EFXP_RZN_SOC_RECEP"]',
+      purchaseType: 'select[name="EFXP_TIPOCOMPRA_SELECT"]',
+      address: 'input[name="EFXP_DIR_RECEP"]',
+      commune: 'input[name="EFXP_CMNA_RECEP"]',
+      city: 'input[name="EFXP_CIUDAD_RECEP"]',
+      concept: 'input[name="EFXP_GIRO_RECEP"]',
+      contact: 'input[name="EFXP_CONTACTO"]',
+      requestRut: {
+        rut: 'input[name="EFXP_RUT_SOLICITA"]',
+        dv: 'input[name="EFXP_DV_SOLICITA"]'
+      }
+    },
+    transport: {
+      rut: {
+        rut: 'input[name="EFXP_RUT_TRANSPORTE"]',
+        dv: 'input[name="EFXP_DV_TRANSPORTE"]'
+      },
+      patent: 'input[name="EFXP_PATENTE"]',
+      driverRut: {
+        rut: 'input[name="EFXP_RUT_CHOFER"]',
+        dv: 'input[name="EFXP_DV_CHOFER"]'
+      },
+      driverName: 'input[name="EFXP_NOMBRE_CHOFER"]'
+    },
+    products: {
+      options: {
+        code: 'input[name="COD_SI_NO"]',
+        additionalTaxes: 'input[name="OTRO_IMP_SI_NO"]',
+        add: 'input[name="AGREGA_DETALLE"]'
+      },
+      type: 'input[name="EFXP_TPO_COD_"]',
+      code: 'input[name="EFXP_COD_"]',
+      name: 'input[name="EFXP_NMB_"]',
+      description: {
+        check: 'input[name="DESCRIP_"]',
+        value: 'textarea[name="EFXP_DSC_ITEM_"]'
+      },
+      quantity: 'input[name="EFXP_QTY_"]',
+      unity: 'input[name="EFXP_UNMD_"]',
+      price: 'input[name="EFXP_PRC_"]',
+      additionalTax: 'select[name="EFXP_OTRO_IMP_"]',
+      discount: 'input[name="EFXP_PCTD_"]'
+    },
+    payment: {
+      type: 'select[name="EFXP_FMA_PAGO"]'
+    },
+    sendBtn: 'button[name="Button_Update"]'
   },
   async scrapeTable(page) {
     const data = await page.$$eval('table > tbody > tr', elements => {
@@ -50,7 +110,7 @@ const scraperObj = {
     return id.split(';')[1].trim();
   },
   async scrapeMulti(page) {
-    const data = {};
+    const data = [];
     const options = await page.$$eval(
       'select[name="RUT_EMP"] optgroup > option',
       opts => opts.map(opt => opt.value)
@@ -60,27 +120,156 @@ const scraperObj = {
       await page.click('button[type="submit"]');
       await page.waitForSelector('.container');
       const id = await this.getID(page);
-      data[id] = await this.scrapeBusiness(page);
+      const result = await this.scrapeBusiness(page);
+      data.push({
+        rut: id,
+        data: result
+      });
       await page.goto(this.multiRoute);
       await page.waitForSelector('.container');
     }
     return data;
   },
-  async scraper({ browser, username, password }) {
-    let data = {};
-    const page = await browser.newPage();
+  async processInput(page, data, selector) {
+    if (data.action === 'click') {
+      await page.click(selector);
+    } else {
+      await page.type(selector, data)
+      if (selector.includes('EFXP_DV_RECEP')) {
+        await page.click('.container');
+        await page.waitForNavigation();
+      }
+    }
+  },
+  async processSelect(page, data, selector) {
+    if (typeof data === 'number') {
+      await page.select(selector, data);
+    } else {
+      const options = await page.$$eval(
+        `${selector} optgroup > option`,
+        opts => opts.map(opt => {
+          return (({ value, text }) => ({ value, text }))(opt);
+        })
+      )
+      for (const option of options) {
+        if (option.text === data) {
+          await page.select(selector, option.value);
+          break;
+        }
+      }
+    }
+  },
+  async processField(...args) {
+    if (args[2].startsWith('input')) {
+      await this.processInput(...args);
+    } else if (args[2].startsWith('select')) {
+      await this.processSelect(...args);
+    }
+  },
+  async parseNum(num) {
+    return num < 10 ? `0${num}` : `${num}`;
+  },
+  async processProducts(page, articles) {
+    const { products } = this.tags;
+    let codeQ = false;
+    let addTx = false;
+    let count = 1;
 
-    await page.goto(this.url);
+    for (const article of articles) {
+      const num = await this.parseNum(count++);
+      const {
+        description,
+        ...product
+      } = article;
+      if (num !== '01') {
+        await page.click(products.options.add);
+        await page.waitFor(100);
+      }
+      if ((product.type || product.code) && !codeQ) {
+        codeQ = !codeQ;
+        await this.processInput(
+          page,
+          { action: 'click' },
+          products.options.code
+        );
+      }
+      if (product.additionalTax && !addTx) {
+        addTx = !addTx;
+        await this.processInput(
+          page,
+          { action: 'click' },
+          products.options.additionalTaxes
+        );
+      }
+      if (description) {
+        const { check, value } = products.description;
+        await this.processInput(
+          page,
+          { action: 'click' },
+          check.replace('"]', `${num}"]`)
+        );
+        await this.processInput(
+          page,
+          description,
+          value.replace('"]', `${num}"]`)
+        );
+      }
+      for (const key of Object.keys(product)) {
+        const selector = products[key].replace('"]', `${num}"]`);
+        await this.processSelectors(page, product[key], `${selector}`);
+      }
+    }
+  },
+  async processSelectors(page, data, selector) {
+    if (typeof data === 'object' && typeof selector === 'object') {
+      for (const key of Object.keys(data)) {
+        await this.processSelectors(page, data[key], selector[key]);
+      }
+    } else {
+      const element = await page.$(selector);
+      if (element) {
+        await this.processField(page, data, selector);
+      }
+    }
+  },
+  async createDocument({ document, browser, ...params }) {
+    const page = await this.login((await browser.newPage()), params);
+    const { products, ...rest } = document;
+    for (const key of Object.keys(rest)) {
+      await this.processSelectors(page, rest[key], this.tags[key])
+    }
+    await this.processProducts(page, products);
+    if (!params.debug) {
+      await page.click(this.tags.sendBtn);
+      await page.waitForNavigation();
+    }
+  },
+  async login(page, { url, username, password }) {
+    await page.goto(url);
     await page.type('#rutcntr', username);
     await page.type('#clave', password);
     await page.click('#bt_ingresar');
     await page.waitForSelector('.container');
-    console.log('scraping business 1');
-    if (page.url() === this.uniqueRoute) {
+    if (page.url().startsWith('https://www1.sii.cl/cgi-bin/Portal001'))
+      console.log('login successfull');
+    else
+      console.log('login failed');
+    return page;
+  },
+  async scraper({ browser, ...params }) {
+    let data = [];
+    const page = await this.login((await browser.newPage()), params);
+
+    if (page.url().includes('Portal001/mipeAdmin')) {
       const id = await this.getID(page);
-      data[id] = await this.scrapeBusiness(page);
       console.log('scraping unico', id)
-    } else if (page.url() === this.multiRoute) {
+      const result = await this.scrapeBusiness(page);
+      data.push({
+        rut: id,
+        data: result
+      })
+    } else if (page.url().includes('mipeSelEmpresa')) {
+      this.multiRoute = page.url();
       console.log('scraping multiple');
       data = await this.scrapeMulti(page);
     }
@@ -112,7 +301,27 @@ const scrapeAll = async ({ rut: username, clave: password, url }) => {
     const result = await scraperObj.scraper({
       browser,
       username,
-      password
+      password,
+      url
+    });
+    browser.close();
+    return result;
+  }
+  catch (err) {
+    browser.close();
+    throw err;
+  }
+}
+
+const createDocument = async ({ rut: username, clave: password, ...params }) => {
+  let browser;
+  try {
+    browser = await startBrowser();
+    const result = await scraperObj.createDocument({
+      browser,
+      username,
+      password,
+      ...params
     });
     browser.close();
     return result;
@@ -124,10 +333,51 @@ const scrapeAll = async ({ rut: username, clave: password, url }) => {
 }
 
 const getEmited = async (settings) => {
-  const result = await scrapeAll(settings);
+  const url = 'https://zeusr.sii.cl/AUT2000/InicioAutenticacion/IngresoRutClave.html?https://www1.sii.cl/cgi-bin/Portal001/mipeSelEmpresa.cgi?DESDE_DONDE_URL=OPCION%3D2%26TIPO%3D4';
+  const result = await scrapeAll({url, ...settings});
+  return result;
+}
+
+const getReceived = async (settings) => {
+  const url = 'https://zeusr.sii.cl/AUT2000/InicioAutenticacion/IngresoRutClave.html?https://www1.sii.cl/cgi-bin/Portal001/mipeSelEmpresa.cgi?DESDE_DONDE_URL=OPCION%3D1%26TIPO%3D4';
+  const result = await scrapeAll({ url, ...settings });
+  return result;
+}
+
+const createAffectInvoice = async (settings, document) => {
+  const url = 'https://zeusr.sii.cl/AUT2000/InicioAutenticacion/IngresoRutClave.html?https://www1.sii.cl/cgi-bin/Portal001/mipeSelEmpresa.cgi?DESDE_DONDE_URL=OPCION%3D33%26TIPO%3D4';
+  const result = await createDocument({
+    document,
+    url,
+    ...settings
+  });
+  return result;
+}
+
+const createExemptInvoice = async (settings, document) => {
+  const url = 'https://zeusr.sii.cl/AUT2000/InicioAutenticacion/IngresoRutClave.html?https://www1.sii.cl/cgi-bin/Portal001/mipeSelEmpresa.cgi?DESDE_DONDE_URL=OPCION%3D34%26TIPO%3D4';
+  const result = await createDocument({
+    document,
+    url,
+    ...settings
+  });
+  return result;
+}
+
+const createDispatchGuide = async (settings, document) => {
+  const url = 'https://zeusr.sii.cl/AUT2000/InicioAutenticacion/IngresoRutClave.html?https://www1.sii.cl/cgi-bin/Portal001/mipeSelEmpresa.cgi?DESDE_DONDE_URL=OPCION%3D52%26TIPO%3D4';
+  const result = await createDocument({
+    document,
+    url,
+    ...settings
+  });
   return result;
 }
 
 module.exports = {
-  getEmited
+  getEmited,
+  getReceived,
+  createAffectInvoice,
+  createExemptInvoice,
+  createDispatchGuide
 };
